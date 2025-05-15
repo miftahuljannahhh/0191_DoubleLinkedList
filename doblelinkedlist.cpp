fc3abc4b926ef8467246d95b424c5fe62297b947
miftahuljannahhh <mj5416386@gmail.com>
2025-05-15 11:48:04 +0700

    void traverse

--- doblelinkedlist.cpp
+++ doblelinkedlist.cpp
@@ -23,11 +23,11 @@ public:
 
     void addNode()
     {
-         int nim;
-         string nm;
+        int nim;
+        string nm;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
-        
+
         // Step 1: Allocate memory for new node
         Node *newNode = new Node();
 
@@ -35,7 +35,7 @@ public:
         newNode->noMhs = nim;
 
         // Step 3: Insert at beginning if list is empty or nim is smallest
-        if (START == NULL || nim <= START->noMhs) 
+        if (START == NULL || nim <= START->noMhs)
         {
             if (START != NULL && nim == START->noMhs)
             {
@@ -43,10 +43,11 @@ public:
                 return;
             }
             // Step 4: newNode.next = START
-            newNode->next=  START;
+            newNode->next = START;
 
             // Step 5: START.prev = newNode (if START exist)
-            if (START != NULL);
+            if (START != NULL)
+                ;
             START->prev = newNode;
 
             // Step 6: newNode.prev = NULL
@@ -65,7 +66,7 @@ public:
             current = current->next;
         }
 
-        if (current->next !=  NULL && nim == current->next->noMhs)
+        if (current->next != NULL && nim == current->next->noMhs)
         {
             cout << "\nDuplicate rollnumbers not allowed" << endl;
             return;
@@ -73,21 +74,21 @@ public:
 
         // Step 9: Insert between current and current->next
         newNode->next = current->next; // Step 9a: newNode.next = current.next
-        newNode->prev = current;      // Step 9b: newNode.prev = current
+        newNode->prev = current;       // Step 9b: newNode.prev = current
 
         // insert last node
         if (current->next != NULL)
             current->next->prev = newNode; // Step 9c: current.next,prev = newNode
 
-            current->next = newNode; // Step 9d: current.next = newNode
+        current->next = newNode; // Step 9d: current.next = newNode
     }
 
     void hapus()
     {
         if (START == NULL)
         {
-           cout << "\nList is empty" << endl;
-           return;
+            cout << "\nList is empty" << endl;
+            return;
         }
 
         cout << "\nEnter the roll number of the student whose record is to be delated: ";
@@ -100,35 +101,54 @@ public:
         while (current != NULL && current->noMhs != rollNo)
             current = current->next;
 
-            if (current == NULL)
-            {
-                cout << "Record not found" << endl;
-                return;
-            }
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
 
-            // Step 2: If node is at the beginning
-            if (current == START)
-            {
-                START = current->next; // Step 2a: START = START.next
-                if  (START != NULL)
-                    START->prev = NULL; // Step 2b:  START.prev = NULL   
-            }
-            else 
-            {
-                // Step 3: Link previous node to next of current
-                current->prev->next = current->next;
-                
-                // Step 4: If current is not the last node
-                if (current->next != NULL)
+        // Step 2: If node is at the beginning
+        if (current == START)
+        {
+            START = current->next; // Step 2a: START = START.next
+            if (START != NULL)
+                START->prev = NULL; // Step 2b:  START.prev = NULL
+        }
+        else
+        {
+            // Step 3: Link previous node to next of current
+            current->prev->next = current->next;
+
+            // Step 4: If current is not the last node
+            if (current->next != NULL)
                 current->next->prev = current->prev;
-            }
+        }
 
-            //Step 5: Delete the node
-            delete current;
-            cout << "Record with roll number " << rollNo << " deleted" << endl;
+        // Step 5: Delete the node
+        delete current;
+        cout << "Record with roll number " << rollNo << " deleted" << endl;
     }
 
-    
-   
-};
+    void traverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+        // step 1: Mark first node as currentNode
+        Node *currentNode = START;
+        // step 2: Repeat until currentNode == NULL
+        cout << "\nRecords in ascending order of roll number are:\n";
+        int i = 0;
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+            // step 3: Move to next node
+            currentNode = currentNode->next;
+            i++;
+        }
+    }
 
+   
+}
