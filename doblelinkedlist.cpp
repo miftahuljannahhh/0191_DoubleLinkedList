9a3630c74235b12c7e4d1f6591f0d8fd2c283c48
miftahuljannahhh <mj5416386@gmail.com>
2025-05-15 11:49:27 +0700

    void retraverse

--- doblelinkedlist.cpp
+++ doblelinkedlist.cpp
@@ -150,5 +150,34 @@ public:
         }
     }
 
-   
+    void revtraverse()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        // step 1: Move to last node
+        Node *currentNode = START;
+        int i = 0;
+        while (currentNode->next != NULL)
+        {
+            currentNode = currentNode->next;
+            i++;
+        }
+
+        // step 2: traverse backward
+        cout << "\nRecords in descending order of roll number are: \n";
+        while (currentNode != NULL)
+        {
+            cout << i + 1 << ". " << currentNode->noMhs << " " << endl;
+
+            // step 3: Move to previous node
+            currentNode = currentNode->prev;
+            i--;
+        }
+    }
+
+    
 }
