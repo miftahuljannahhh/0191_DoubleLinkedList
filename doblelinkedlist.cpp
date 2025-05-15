d1c81be962e1b2d9d23bf406d85f9cd48587480d
miftahuljannahhh <mj5416386@gmail.com>
2025-05-15 10:04:08 +0700

    Doublelinkedlist

--- doblelinkedlist.cpp
+++ doblelinkedlist.cpp
@@ -4,8 +4,44 @@ using namespace std;
 
 class Node
 {
+public:
     int noMhs;
     Node *next;
     Node *prev;
+};
+
+class DoubleLinkedList
+{
+private:
+    Node *START;
+
+public:
+    DoubleLinkedList()
+    {
+        START = NULL;
+    }
+
+    void addNode()
+    {
+         int nim;
+         string nm;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+        
+        // Step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        // Step 2: Assignvalue to the data fields
+        newNode->noMhs = nim;
+
+        // Step 3: Insert at beginning if list is empty or nim is smallest
+        if (START == NULL || nim <= START->noMhs) 
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number of allowed" << endl;
+            }
+        }
+    }
 };
 
